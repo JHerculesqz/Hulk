2e3c2694c833afc27a880524e0b1597f42ce0c9a
JHercules <JHercules_qz@qq.com>
2018-10-30 20:43:11 +0800

    update ES Example

---
+++ core-fw-spring-test-db-es/src/main/java/com/firelord/CoreFwSpringTestDbEsApplication.java
@@ -0,0 +1,11 @@
+package com.firelord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreFwSpringTestDbEsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreFwSpringTestDbEsApplication.class, args);
+    }
+}
---
+++ core-fw-spring-test-db-es/src/main/java/com/firelord/db/es/dao/TBLBook.java
@@ -0,0 +1,29 @@
+package com.firelord.db.es.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(indexName = "testdb", type = "tblBook")
+public class TBLBook {
+    @Id
+    @Setter
+    @Getter
+    private String id;
+
+    @Setter
+    @Getter
+    private String name;
+
+    @Setter
+    @Getter
+    private String type;
+
+    @Setter
+    @Getter
+    private List<CellMo> cellMos = new ArrayList<>();
+}
