4a1c59726a77f04c605f8905ab436c03ba1a7591
JHercules <JHercules_qz@qq.com>
2018-12-05 16:48:45 +0800

    update project

--- core-fw-opencv/src/main/java/com/firelord/opencv/matrix/VisionMatrixInit.java
+++
@@ -1,5 +0,0 @@
-package com.firelord.opencv.matrix;
-
-public enum VisionMatrixInit {
-    EYE, COPY
-}
--- core-fw-opencv/src/main/java/com/firelord/opencv/video/VisionVideo4PCCallback.java
+++
@@ -1,9 +0,0 @@
-package com.firelord.opencv.video;
-
-public interface VisionVideo4PCCallback {
-    void whenOpenSuccess();
-
-    void whenGrabImgFrame();
-
-    void whenOpenFail();
-}
