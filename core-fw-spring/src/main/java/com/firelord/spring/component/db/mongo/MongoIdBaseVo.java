783b4b1a830b568af94f808ba1e6e2097258c50b
JHercules <JHercules_qz@qq.com>
2019-03-12 17:12:55 +0800

    Hulk-core-fw-update RandomUtilsEx、addMogoIdBaseVo

---
+++ core-fw-spring/src/main/java/com/firelord/spring/component/db/mongo/MongoIdBaseVo.java
@@ -0,0 +1,16 @@
+package com.firelord.spring.component.db.mongo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+public class MongoIdBaseVo {
+    //#region Fields
+
+    @Setter
+    @Getter
+    private String id;
+
+    //#endregion
+}
--- core-fw/src/main/java/com/firelord/component/ds/random/RandomUtilsEx.java
+++ core-fw/src/main/java/com/firelord/component/ds/random/RandomUtilsEx.java
@@ -3,6 +3,11 @@ package com.firelord.component.ds.random;
 
 import org.apache.commons.lang3.RandomUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RandomUtilsEx {
     //#region next
 
@@ -16,6 +21,33 @@ public class RandomUtilsEx {
 
     public static double nextDouble(double iMin, double iMax) {
         return RandomUtils.nextDouble(iMin, iMax);
+    }
+
+    public static <T> List<T> nextList(int iCount, List<T> lstTarget) {
+        List<T> lstRes = new ArrayList<>();
+
+        //if lstTarget size less than iCount
+        if (lstTarget.size() <= iCount) {
+            for (T oT : lstTarget) {
+                lstRes.add(oT);
+            }
+        }
+        //if lstTarget size greater than iCount
+        else {
+            Map<Integer, String> mapRandomIndex = new HashMap<>();
+
+            while (mapRandomIndex.size() < iCount) {
+                int iRandomIndex = (int) (Math.random() * lstTarget.size());
+                if (!mapRandomIndex.containsKey(iRandomIndex)) {
+                    mapRandomIndex.put(iRandomIndex, "");
+                    lstRes.add(lstTarget.get(iRandomIndex));
+                }
+            }
+
+            mapRandomIndex.clear();
+        }
+
+        return lstRes;
     }
 
     //#endregion
